08d070caaf8163e0bb1b3ae764f2a90f8d51c584
Haimeng Zhou <hzhou@teladoc.com>
2016-02-05 17:27:45 -0500

    add a exception action and use ruler 0.1.2.3

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,13 @@
 class QuotesController < Rulers::Controller
   def a_quote
     "There is nothing either good or bad " +
-      "but thinking makes it so."
+      "but thinking makes it so." +
+      "\n<pre>\n#{env}\n</pre>"
+  end
+
+  class QuotesController < Rulers::Controller
+    def exception
+      raise "It's a bad one!"
+    end
   end
 end
