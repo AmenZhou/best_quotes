e5a0af63d92a8af2577ea184ba742d966c509819
Haimeng Zhou <hzhou@teladoc.com>
2016-02-12 09:36:17 -0500

    remove unused require

--- config/application.rb
+++ config/application.rb
@@ -3,9 +3,10 @@ require "rulers"
 $LOAD_PATH << File.join(File.dirname(__FILE__),
                         "..", "app",
                         "controllers")
-require "quotes_controller"
-require "home_controller"
 
+$LOAD_PATH << File.join(File.dirname(__FILE__),
+                        "..", "app",
+                        "models")
 module BestQuotes
   class Application < Rulers::Application
   end
