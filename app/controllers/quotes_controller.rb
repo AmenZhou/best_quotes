10fa1bf449682d2186a5a8ef1721d458673796fb
Haimeng Zhou <hzhou@teladoc.com>
2016-03-01 10:02:55 -0500

    chapter_5: add FileModel.all

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,9 @@
 class QuotesController < Rulers::Controller
+  def index
+    quotes = FileModel.all
+    render :index, quotes: quotes
+  end
+
   def quote_1
     quote_1 = FileModel.find(1)
     render :quote, obj: quote_1
